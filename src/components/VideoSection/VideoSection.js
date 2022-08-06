4779af10d96c1ba08bcca4d7fc9a7def41005e79
HaoNguyen <hao.nguyen@ucars.sg>
2022-08-06 09:43:38 +0700

    fix video

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -5,7 +5,7 @@ const VideoSection = () => {
   return (
     <div className="video-section">
       <h3>Turn max volume and feel it =))))))</h3>
-      <ReactPlayer url="https://www.youtube.com/watch?v=pn3-uZtt_N8&ab_channel=H%E1%BA%A3oH%E1%BB%AFu" />
+      <ReactPlayer url="https://www.youtube.com/watch?v=pn3-uZtt_N8" />
     </div>
   );
 };
