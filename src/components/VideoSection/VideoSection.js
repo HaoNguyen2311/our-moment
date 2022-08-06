606ad7f304eb1852a6953508a9b04d98c329de7a
HaoNguyen <hao.nguyen@ucars.sg>
2022-08-06 14:07:05 +0700

    fix video round 4

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
 import "./VideoSection.scss";
 
 const VideoSection = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const onPlay = (e) => {
+    setIsPlaying(true);
+  };
+  const onPause = (e) => {
+    setIsPlaying(false);
+  };
+  const onEnded = (e) => {
+    setIsPlaying(false);
+  };
   return (
     <div className="video-section">
-      <a href="https://youtu.be/pn3-uZtt_N8">Click me</a>
       <h3>Turn max volume and feel it =))))))</h3>
-      <ReactPlayer url="https://www.youtube.com/watch?v=pn3-uZtt_N8" />
+      <ReactPlayer
+        onPlay={onPlay}
+        onPause={onPause}
+        onEnded={onEnded}
+        muted={true}
+        url="https://youtu.be/85zV_-djU9E"
+      />
+      <div className="music">
+        <ReactPlayer
+          playing={isPlaying}
+          url="https://www.youtube.com/watch?v=X-y4oROr_Zw&ab_channel=TheVibeGuide"
+        />
+      </div>
     </div>
   );
 };
