caa9328e02359ae01d839576c8bbc0feee77f083
HaoNguyen <hao.nguyen@ucars.sg>
2022-08-06 16:48:42 +0700

    fix video

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,13 +1,13 @@
-// import { Modal } from "antd";
+import { Modal } from "antd";
 import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
-// import GiftIcon from "./GiftIcon/GiftIcon";
+import GiftIcon from "./GiftIcon/GiftIcon";
 import "antd/dist/antd.min.css";
 import "./VideoSection.scss";
 
 const VideoSection = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  // const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const onPlay = (e) => {
     setIsPlaying(true);
   };
@@ -17,34 +17,35 @@ const VideoSection = () => {
   const onEnded = (e) => {
     setIsPlaying(false);
   };
-  // const onClickOpenModal = () => {
-  //   setIsModalVisible(true);
-  // };
-  // const onCloseModal = () => {
-  //   setIsModalVisible(false);
-  //   setIsPlaying(false);
-  // };
+  const onClickOpenModal = () => {
+    setIsModalVisible(true);
+  };
+  const onCloseModal = () => {
+    setIsModalVisible(false);
+    setIsPlaying(false);
+  };
 
   return (
     <div className="video-section">
-      {/* <GiftIcon onClick={onClickOpenModal} />
+      <GiftIcon onClick={onClickOpenModal} />
       <Modal
         footer={null}
         wrapClassName="video-modal"
         title="Turn max volume and feel it =))))))"
         visible={isModalVisible}
         onCancel={onCloseModal}
-      > */}
-      <ReactPlayer
-        playing={isPlaying}
-        width="100%"
-        onPlay={onPlay}
-        onPause={onPause}
-        onEnded={onEnded}
-        muted={true}
-        url="https://youtu.be/85zV_-djU9E"
-      />
-      {/* </Modal> */}
+      >
+        <ReactPlayer
+          controls
+          playing={isPlaying}
+          width="100%"
+          onPlay={onPlay}
+          onPause={onPause}
+          onEnded={onEnded}
+          muted={true}
+          url="https://youtu.be/85zV_-djU9E"
+        />
+      </Modal>
 
       <div className="music">
         <ReactPlayer
