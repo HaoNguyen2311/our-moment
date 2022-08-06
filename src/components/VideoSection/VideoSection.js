10c76823cd15972cd9110144a7f37185dd998724
HaoNguyen <hao.nguyen@ucars.sg>
2022-08-06 10:11:21 +0700

    try video

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -4,6 +4,7 @@ import "./VideoSection.scss";
 const VideoSection = () => {
   return (
     <div className="video-section">
+      <a href="https://youtu.be/pn3-uZtt_N8">Click me</a>
       <h3>Turn max volume and feel it =))))))</h3>
       <ReactPlayer url="https://www.youtube.com/watch?v=pn3-uZtt_N8" />
     </div>
